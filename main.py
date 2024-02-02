f5cefedb3f0d80e93f15a9a44020e6215b786064
Joshua Butler <joshuabutler798@gmail.com>
2024-02-02 15:50:57 -0400

    Adding basic comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#connects to the mathFile.py file and uses the functions 
 import mathFile
 
 print ("Choose from the list below:")
@@ -7,12 +7,14 @@ print ("2. Subtract")
 print ("3. Multiply")
 print ("4. Divide")
 
+#gets the input for the math functions
 choice = int(input("Enter choice(1/2/3): "))
 
-
+#gets the input for math functions
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+#calls the functions from the mathFile.py file
 if choice == 1:
     print (num1,"+",num2,"=", mathFile.add(num1,num2))
 
--- mathFile.py
+++ mathFile.py
@@ -1,9 +1,12 @@
 
+# This file contains the functions for the calculator
 
+# This function adds two numbers
 def add(x,y):
     return x+y
 
+# This function subtracts two numbers
 def subtract(x,y):
     
-
+# This function multiplies two numbers
 def multiply(x,y):
