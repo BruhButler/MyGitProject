dfd14fdf408d6e30253dc6bc3842391e092550e8
SHAKIL-87 <157614595+SHAKIL-87@users.noreply.github.com>
2024-02-03 13:11:15 -0400

    Add files via upload
    I added the subtract, multiply and divide options. Also added \n before enter choice option for flourishment.

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ print ("Choose from the list below:")
 print ("1. Add")
 print ("2. Subtract")
 print ("3. Multiply")
-print ("4. Divide")
+print ("4. Divide" "\n")
 
 #gets the input for the math functions
-choice = int(input("Enter choice(1/2/3): "))
-
+choice = int(input("Enter choice(1/2/3/4): "))
+   
 #gets the input for math functions
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -19,15 +19,10 @@ if choice == 1:
     print (num1,"+",num2,"=", mathFile.add(num1,num2))
 
 elif choice == 2:
-    print ()
+    print (num1,"-",num2,"=", mathFile.subtract(num1,num2))
 
 elif choice == 3:
-    print ()
+    print (num1,"*",num2,"=", mathFile.multiply(num1,num2))
 
-
-
-    
-
-
-
-
+elif choice == 4:
+    print (num1,"/",num2,"=", mathFile.divide(num1,num2))
